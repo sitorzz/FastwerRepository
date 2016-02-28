c917ccff6c3a8ddc85e4aeb2ab90b42355c832b6
oriolandresibanez <oriolandres@MacBook-Pro-de-Oriol.local>
2016-02-28 21:12:07 +0100

    2

--- Fastwer/alta_pregunta.php
+++ Fastwer/alta_pregunta.php
@@ -33,48 +33,205 @@ $connect  = mysqli_connect("localhost","root","root","fastwer_db") or die("Error
 
 <body>
 
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    <img src="http://placehold.it/150x50&text=Logo" alt="">
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="home.php">Visualizar</a>
+                    </li>
+                    <li>
+                        <a href="alta_pregunta.php">Nueva Pregunta</a>
+                    </li>
+                    <li>
+                        <a href="myFriends.php">Amigos</a>
+                    </li>
+                    <li>
+                        <a href="#">Soporte</a>
+                    </li>
+                    <li>
+                        <a href="#">Mi perfil</a>
+                    </li>
+                    <li>
+                        <a href="php/logout.php">Cerrar sesión</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Heading Row -->
+        <div class="row">
+            <div class="col-xs-12">
+                <img class="img-responsive img-rounded" src="http://placehold.it/900x350" alt="">
+            </div>
+            <!-- /.col-md-8 -->
+            <div class="col-xs-12">
+                <h1>Alta pregunta</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            </div>
+            <!-- /.col-md-4 -->
+        </div>
+        <!-- /.row -->
+
+        <hr>
 
         <!-- Call to Action Well -->
         <div class="row">
-            
+           
            <form method="POST">
+
+              <div class="form-group col-xs-12">
+                <label>* Titulo pregunta:</label>
+                <input type="text" class="form-control" placeholder="Texto pregunta" name="titulPreg" required />
+              </div>
+
               <div class="form-group col-md-6">
-                <label>* Titulo pregunta</label>
-                <input type="text" class="form-control" placeholder="Titulo pregunta" name="tituloPreg"/>
+                <label>Imagen (Url):</label>
+                <input type="text" class="form-control" placeholder="Formato jpg,png" name="imagenPreg"/>
               </div>
 
               <div class="form-group col-xs-12">
                 <label>* Texto:</label>
-                <input type="text" class="form-control" placeholder="Texto pregunta" name="textoPreg" />
+                <input type="text" class="form-control" placeholder="Texto pregunta" name=textoPreg required/>
               </div>
 
               </br>
 
+              <div class="form-group col-xs-2">
+              	<label>Tipo simple: (Si/No)  </label>
+                <input type="checkbox" name="respuestaSimpl">
+              </div>
+              <div class="form-group col-xs-10">
+              <p>(En caso de no ser simple el tipo de respuesta que quieres, rellena los campos con las possibles respuestas)</p>
+              </div>
+              <div class="form-group col-md-4">
+                <label>Respuesta 1:</label>
+                <input type="textarea" class="form-control" placeholder="Respuesta 1" name="Resp1">
+              </div>
+              <div class="form-group col-md-4">
+                <label>Respuesta 2:</label>
+                <input type="textarea" class="form-control" placeholder="Respuesta 2" name="Resp2">
+              </div>
+              <div class="form-group col-md-4">
+                <label>Respuesta 3:</label>
+                <input type="textarea" class="form-control" placeholder="Respuesta 3" name="Resp3">
+              </div>
 
-
-
-
-                <div class="form-group col-xs-12">
+              <div class="form-group col-xs-12">
                  <input type="submit" class="btn btn-default"></input>
               </div>
-              </form>
 
 
-            <?php
+          </form>
 
-                $tituloPregun = $_POST["tituloPreg"];
-                
-                $textoPregun = $_POST["textoPreg"];
-                
-                var_dump($tituloPregun);
-                var_dump($textoPregun);
+           	<?php
 
-                if(isset($tituloPregun)){
-                    
-                }
+           		if(isset($_POST["titulPreg"])){
 
-            ?>
-    
+        		$tituloPregun = $_POST["titulPreg"];
+        		$imagenPregun = $_POST["imagenPreg"];
+        		$textoPregun = $_POST["textoPreg"];
+        		$respuesta1 = $_POST["Resp1"];
+        		$respuesta2 = $_POST["Resp2"];
+        		$respuesta3 = $_POST["Resp3"];
+
+        		if(isset($_POST["respuestaSimpl"])){
+
+        			$insertRespSimp="insert into question(title,question,votes,image_question,date_create,fk_user,is_simple, views) values('$tituloPregun','$textoPregun',0,'$imagenPregun', NOW(),1,'s',0)";
+
+        			
+        			
+		            if ($connect->query($insertRespSimp) === TRUE) {
+
+						$selectPk2="select id_question from question where question='$textoPregun'";
+						$resultado_consulta_mysql2 = mysqli_query($connect, $selectPk2);
+
+					    while ($row1 = mysqli_fetch_array($resultado_consulta_mysql2)) {
+
+	        			$insertRespost="insert into answer(fk_question,answer) values ('$row1[0]','Si')";
+	        			$insertRespost2="insert into answer(fk_question,answer) values ('$row1[0]','No')";
+	        			$connect->query($insertRespost);
+	        			$connect->query($insertRespost2);
+
+	        			}
+
+		                echo "<div class='col-xs-12'>
+		                		<p>Insert correcto</p>
+		                		</div>";
+
+		            } else {
+		                
+		               
+
+		                echo "<div class='col-xs-12'>
+		                		<p>Insert incorrecto</p>
+		                		</div>";
+		            }
+
+
+        		}else{
+
+        				$insertRespComp="insert into question(title,question,votes,image_question,date_create,fk_user,is_simple, views) values('$tituloPregun','$textoPregun',0,'$imagenPregun', NOW(),1,'n',0)";
+
+		                if ($connect->query($insertRespComp) === TRUE) {
+
+						$selectPk="select id_question from question where question='$textoPregun'";
+						$resultado_consulta_mysql = mysqli_query($connect, $selectPk);
+
+					    while ($row = mysqli_fetch_array($resultado_consulta_mysql)) {
+						
+	        			$insertRespost="insert into answer(fk_question,answer) values ('$row[0]','$respuesta1')";
+	        			$insertRespost2="insert into answer(fk_question,answer) values ('$row[0]','$respuesta2')";
+	        			$insertRespost3="insert into answer(fk_question,answer) values ('$row[0]','$respuesta3')";
+	        			$connect->query($insertRespost);
+	        			$connect->query($insertRespost2);
+	        			$connect->query($insertRespost3);
+		                }
+
+        		}
+        	}
+        }
+        	?>
+
+
+
+        </div>
+        <!-- /.row -->
+
+
+
+
+
+        <!-- /.row -->
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; ProyectoM013 - FASTWER</p>
+                </div>
+            </div>
+        </footer>
 
     </div>
     <!-- /.container -->
