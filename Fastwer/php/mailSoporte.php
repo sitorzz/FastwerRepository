e88622f3443ff08286b78e6927c60485ef02d37e
algibe <albertgb@hotmail.es>
2016-04-10 18:55:59 +0200

    Pantalla soporte

---
+++ Fastwer/php/mailSoporte.php
@@ -0,0 +1,71 @@
+<?php
+
+ini_set("SMTP","localhost");
+ini_set("sendmail_from","dauratgripau@hotmail.es");
+
+if(isset($_POST['email'])) {
+    
+    
+$cuerpo = ' 
+<html> 
+<head> 
+   <title>Prueba de correo</title> 
+</head> 
+<body> 
+<h1>Hola amigos!</h1> 
+<p> 
+<b>Bienvenidos a mi correo electrónico de prueba</b>. Estoy encantado de tener tantos lectores. Este cuerpo del mensaje es del artículo de envío de mails por PHP. Habría que cambiarlo para poner tu propio cuerpo. Por cierto, cambia también las cabeceras del mensaje. 
+</p> 
+</body> 
+</html> 
+'; 
+   
+    
+//para el envío en formato HTML 
+$headers = "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+
+//dirección del remitente 
+$headers .= "From: Miguel Angel Alvarez <pepito@desarrolloweb.com>\r\n"; 
+
+//dirección de respuesta, si queremos que sea distinta que la del remitente 
+$headers .= "Reply-To: mariano@desarrolloweb.com\r\n"; 
+
+//ruta del mensaje desde origen a destino 
+$headers .= "Return-path: holahola@desarrolloweb.com\r\n"; 
+
+//direcciones que recibián copia 
+$headers .= "Cc: maria@desarrolloweb.com\r\n"; 
+
+//direcciones que recibirán copia oculta 
+$headers .= "Bcc: pepe@pepe.com,juan@juan.com\r\n"; 
+    
+    
+
+// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
+$email_to = "dauratgripau@hotmail.es";
+$email_subject = "Contacto desde el sitio web";
+
+// Aquí se deberían validar los datos ingresados por el usuario
+if(!isset($_POST['first_name']) ||
+!isset($_POST['email']) ||
+!isset($_POST['comments'])) {
+
+echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
+echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+die();
+}
+
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre: " . $_POST['first_name'] . "\n";
+$email_message .= "E-mail: " . $_POST['email'] . "\n";
+$email_message .= "Comentarios: " . $_POST['comments'] . "\n\n";
+
+$email_from = "email from";
+    
+
+mail($email_to, $email_subject, $cuerpo, $headers);
+
+echo "¡El formulario se ha enviado con éxito!";
+}
+?>
