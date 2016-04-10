6750eb2ca28c2e4f1200889d8e4497703b4178fc
Ruben Heras <chromecrownmstr@gmail.com>
2016-04-10 22:34:38 +0200

    comit comun

---
+++ Fastwer/legal.php
@@ -0,0 +1,104 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>FastWer, tu aplicación de preguntas y respuestas</title>
+    
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/small-business.css" rel="stylesheet">
+    
+    
+
+    
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    <img src="http://placehold.it/150x50&text=Logo" alt="">
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.php">Servicios</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Registrarse</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Acceder</a>
+                    </li>
+                    <li>
+                        <a href="aboutus.php">Sobre nosotros</a>
+                    </li>
+                    <li>
+                        <a href="#">Pólitica del sitio</a>
+                    </li>
+                    <li>
+                        <a href="#">Contactar</a>
+                    </li>
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    <div class="container-fluid text-center">
+<div class="row jumbotron">
+        <h1>Política e información del sitio web</h1>
+        <h2>Puede consultar nuestra pólitica en todos sus aspectos a traves del siguiente enlace</h2>  
+        <p>Fastwer como plataforma de intercambio de dudas, preguntas e informacion requiere de un registro previo y que apesar de poseer únicamente lo imprescindible para facilitar el uso d ela plataforma conlleva una responsabilidad conforme a sus datos como el nickname o email. Fastwer no facilitará ni compartirá dichos datos con terceros. En el caso de necesitarlos se le enviará un mail informándole del caso. Nos reservamos el derecho de bannear y tomar medidas legales frente al incumplimiento de la siguiente información facilitada. Cualquier contenido sexual, que incite al odio o violencia y de carácter amenazante será severamente sancionado.</p>
+        <a href="txt.html" target="_blank">Visitar política legal del sitio</a>
+        </div>
+</div>
+        
+
+
+    <footer id="footer">
+        <div class="container">
+            <div class="row">
+
+            </div>
+        </div>
+    </footer><!--/#footer-->
+<!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
+
