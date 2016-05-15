285c1b928440fbcb12d8d1ad90f5fc0f63f88d64
Ruben Heras <chromecrownmstr@gmail.com>
2016-05-15 14:07:23 +0200

    password final
    cambio de password satisfactorio

--- Fastwer/php/update_password.php
+++ Fastwer/php/update_password.php
@@ -3,18 +3,17 @@
 include "session.php";
 include "conexion.php";
 
-echo $_POST["password"];
-echo $_POST["confirm_password"];
-
 if ($_POST["password"]==$_POST["confirm_password"]) {
         
 
 
-$result = "UPDATE user SET password = '".$_POST["password"]."' where id = '".$id_session."'";
+$result = "UPDATE user SET password = '".$_POST["password"]."' where id = $id_session";
 
 $con->query($result);
 
 $con->close();
+
+    echo "Password updated";
 
     } else {
     
